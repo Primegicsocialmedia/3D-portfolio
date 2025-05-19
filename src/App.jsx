416175521c6eef6c4e4cac2b037829e19fff8a66
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-19 16:20:40 +0200

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="centerData h-screen">
+      <h1 className="text-5xl font-bold">Start your Project </h1>
+    </div>
+  );
+};
+
+export default App;
