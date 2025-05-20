26b9e49e21f6972ad1285f2107f95af158bbe690
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-20 14:21:06 +0200

    hero right updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-import
-
-  { words } from "../constants";
+import HeroExperience from "../components/HeroModels/HeroExperience";
+import { words } from "../constants";
 import Button from "../components/Button";
 const Hero = () => {
   return (
@@ -23,26 +21,39 @@ const Hero = () => {
                         key={word.text}
                         className="flex items-center md:gap-3 gap-1 pb-2"
                       >
-                        <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
                         <span>{word.text}</span>
                       </span>
                     ))}
                   </span>
                 </span>
               </h1>
-              
+
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
-            <p className="text-white-50 md:text-xl relative z-10"> We are Prime Creative, a media agency based in East London, South Africa.</p>
-             <Button className="md:w-80 md:h-16 w-60 h-12" 
-             id="button"
-             text="See my Work" 
-             />
+            <p className="text-white-50 md:text-xl relative z-10">
+              {" "}
+              We are Prime Creative, a media agency based in East London, South
+              Africa.
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my Work"
+            />
           </div>
-         
         </header>
-        {/* Right: Hero Content */}
+        {/* Right: #D Model */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
