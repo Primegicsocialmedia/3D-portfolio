cd850098209793201440f398cdd9978fbaf6e814
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-20 15:37:01 +0200

    added hero lights

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,8 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { useMediaQuery } from "react-responsive";
+import { Room } from "./Room";
+import HeroLights from "./HeroLights";
 ;
 
 const HeroExperience = () => {
@@ -12,8 +14,7 @@ const isMobile = useMediaQuery({query: '(max-width: 768px)'});
 
   return (
 <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-    <ambientLight intensity={0.2}  color="#1a1a40"/>
-    <directionalLight position={[5, 5, 5]} intensity={5} />
+    
     <OrbitControls
     enablePan={false}
     enableZoom={!isTablet}
@@ -22,10 +23,14 @@ const isMobile = useMediaQuery({query: '(max-width: 768px)'});
     minPolarAngle={Math.PI / 5}
     maxPolarAngle={Math.PI / 2}
     />
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="teal" />
-    </mesh>
+    <HeroLights />
+    <group
+    scale={isMobile ? 0.7 : 1}
+    position= {[0, -3.5, 0]}
+    rotation={[0,-Math.PI / 4, 0]}
+    >
+      <Room />
+    </group>
 </Canvas>
   );
 };
